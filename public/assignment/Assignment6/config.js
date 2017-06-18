a94ca3a07ce7ceb575f2b70d7052bfb17fc85e4c
Mohib Azam <mohibazam1@gmail.com>
2017-06-18 18:16:14 -0400

    added redirection to login page

--- public/assignment/Assignment6/config.js
+++ public/assignment/Assignment6/config.js
@@ -41,66 +41,110 @@
             .when('/user/:userId/website', {
                 templateUrl: 'views/website/templates/website-list.view.client.html'
                 ,controller: 'websiteListController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/new', {
                 templateUrl: 'views/website/templates/website-new.view.client.html',
                 controller: 'websiteNewController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId', {
                 templateUrl: 'views/website/templates/website-edit.view.client.html',
                 controller: 'websiteEditController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
 
             // page routing
             .when('/user/:userId/website/:websiteId/page', {
                 templateUrl: 'views/page/templates/page-list.view.client.html',
                 controller: 'pageListController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId/page/new', {
                 templateUrl: 'views/page/templates/page-new.view.client.html',
                 controller: 'pageNewController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId/page/:pageId', {
                 templateUrl: 'views/page/templates/page-edit.view.client.html',
                 controller: 'pageEditController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
 
             // widget routing
             .when('/user/:userId/website/:websiteId/page/:pageId/widget', {
                 templateUrl: 'views/widget/templates/widget-list.view.client.html',
                 controller: 'widgetListController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget/new', {
                 templateUrl: 'views/widget/templates/widget-chooser.view.client.html',
                 controller: 'widgetChooseController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget/:widgetId', {
                 templateUrl: 'views/widget/templates/widget-edit.view.client.html',
                 controller: 'widgetEditController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
             .when('/user/:userId/website/:websiteId/page/:pageId/widget/:widgetId/search', {
                 templateUrl: 'views/widget/templates/widget-flickr-search.view.client.html',
                 controller: 'flickrImageSearchController',
-                controllerAs: 'model'
+                controllerAs: 'model',
+                resolve: {
+                    currentUser: checkLoggedIn
+                }
             })
 
             //All else fails, go back to the login page
             .otherwise({redirectTo:'/'});
     }
 
-    function checkLoggedIn(userService) {
+    function checkLoggedIn($q, $location, userService) {
+        var deferred = $q.defer();
         //this, and all other resolve functions, will return a promise
         //The navigation will proceed once all promises are resolved
-        return userService.checkLoggedIn();
+        userService
+            .checkLoggedIn()
+            .then(function (currentUser) {
+                //reject promise if user is '0',
+                //indicating it's invalid
+                if(currentUser === '0') {
+                    deferred.reject();
+                    $location.url('/login');
+                }
+                else {
+                    deferred.resolve(currentUser);
+                }
+            });
+        return deferred.promise;
     }
 
 })();
